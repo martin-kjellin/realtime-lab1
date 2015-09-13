73814c044785f74415fdf2724e1485f856115c49
Jia-Ying Lin <jili3568@siegbahn.it.uu.se>
2015-09-13 17:38:11 +0200

    added command

--- rts_lab1_3.adb
+++ rts_lab1_3.adb
@@ -6,40 +6,40 @@ procedure rts_lab1_3 is
    package Integer_Random is new Ada.Numerics.Discrete_Random(Integer);
    use Integer_Random;
    
-   task Buffer is
+   task Buffer is -- Declarations of the Buffer
       entry Put(X : in integer);
       entry Get(X : out integer);
       entry Stop;
    end;
    
-   task Producer is
+   task Producer is -- Declarations of the Producer
       entry Stop;
    end;
    
-   task Consumer;
+   task Consumer; -- Declarations of the Consumer
    
-   task body Buffer is
-      Size : constant Integer := 15;
-      B : array (1..Size) of Integer;
-      Front : Integer := 0;
-      Number : Integer range 0..Size := 0;
-      Running : Boolean := True;
+   task body Buffer is -- Definition of Buffer task
+      Size : constant Integer := 15; -- Size of the Buffer
+      B : array (1..Size) of Integer; -- Container of the Buffer
+      Front : Integer := 0; -- Point to the top position of the Buffer
+      Number : Integer range 0..Size := 0; -- The number of integers in the Buffer
+      Running : Boolean := True; -- keep running Buffer(True) or not(False)
    begin
       while Running = True loop
 	 select
-	    when Number < Size =>
+	    when Number < Size => -- if the Buffer is not full
 	       accept Put(X : in Integer) do
-		  B((Front + Number) mod Size + 1) := X;
-		  Number := Number + 1;
+		  B((Front + Number) mod Size + 1) := X; -- put x on the bottom 
+		  Number := Number + 1; -- The number of integers in the Buffer plus one
 	       end Put;
 	       
 	 or
 
-	    when Number > 0 =>
+	    when Number > 0 => -- if the Buffer is not empty
 	       accept Get(X : out Integer) do 
-		  X := B(Front mod Size + 1);
-		  Number := Number - 1;
-		  Front := Front + 1;
+		  X := B(Front mod Size + 1); -- get x from the top of the buffer
+		  Number := Number - 1; -- The number of integers in the Buffer minus one 
+		  Front := Front + 1; -- Point to the next top position of the Buffer
 	       end Get;
 	    
 	 or
@@ -52,40 +52,39 @@ procedure rts_lab1_3 is
       end loop;
    end buffer;
    
-   task body Producer is
+   task body Producer is -- Definition of Producer task
       G : Generator;
-      Produce_Range : constant Integer := 26;
-      New_Value : Integer;
-      Running : Boolean := True;
+      Produce_Range : constant Integer := 26; -- maximun of the random number
+      New_Value : Integer; -- New random number produced
+      Running : Boolean := True; -- keep running Producer(True) or not(False)
    begin
       Reset(G);
       while Running = True loop
 	 select
-	    
 	    accept Stop do
 	       Running := False;
 	    end Stop;
-	    
+	   
 	 else
 	    
-	    New_Value := Random(G) mod Produce_Range;
-	    Put("To buffer:");
+	    New_Value := Random(G) mod Produce_Range; -- Produce random number from 0 to 25
+	    Put("Producer Put to buffer:");
 	    Put_Line(Integer'Image(New_Value));
-	    Buffer.Put(New_Value);
+	    Buffer.Put(New_Value); -- Put number into buffer
 	    
 	 end select;
       end loop;
    end Producer;
    
-   task body Consumer is
+   task body Consumer is -- Definition of Producer task
       Sum : Integer := 0;
       New_Value : Integer;
    begin
-      while Sum <= 100 loop
-	 Buffer.Get(New_Value);
-	 Put("From buffer:");
+      while Sum <= 100 loop -- loop while summerizes in consumer no more than 100
+	 Buffer.Get(New_Value); -- get number from buffer
+	 Put("Consumer get from buffer:");
 	 Put_Line(Integer'Image(New_Value));
-	 Sum := Sum + New_Value;
+	 Sum := Sum + New_Value; -- summerized
       end loop;
       Producer.Stop;
       Buffer.Stop;
