65ee6880ee9202b735458e2116aa4375d959aa4e
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-13 14:41:28 +0200

    Added a consumer task.

--- rts_lab1_3.adb
+++ rts_lab1_3.adb
@@ -9,35 +9,52 @@ use Ada.Numerics.Float_Random;
 
 procedure rts_lab1_3 is
 	task buffer is
-		entry put(X: in integer)
-		entry get(x: out integer)
+		entry put(X: in integer);
+		entry get(x: out integer);
 	end;
 	task body buffer is
-		b: array (0 .. 15);
-		front: integer := 0;
+		b: array (0 .. 15) of Integer;
+		front : integer := 0;
 		number: integer := 0;
 		size: integer :=15;
+		Pointer : Integer := 0;
 	begin
 		loop 
 		accept put(x: in integer)
 			do
-				if(number < size)
+				if(number < size) then
 					b(pointer):= x;
-					pointer = (pointer + 1) mod size;
+					pointer := (pointer + 1) mod size;
 				else
-					Put_Line("Buffer Full");
+				   Put_Line("Buffer Full");
+				end if;
 			end put;
 		accept get(x: out integer) 
-			do 
-				if(pointer < size)
+			do
+				if(pointer < size) then
 					b(pointer):= x;
-					pointer = (pointer + 1) mod size;
+					pointer := (pointer + 1) mod size;
 				else
-					Put_Line("Buffer Full");
+				   Put_Line("Buffer Full");
+				end if;
 			end get;
 		end loop;
 	end buffer;
 
-begin 
+	task Consumer is
+	   -- do something
+	end;
+	
+	task body Consumer is
+	   Sum : Integer := 0;
+	   New_Value : Integer;
+	begin
+	   while Sum <= 100 loop
+	      Buffer.Get(New_Value);
+	      Sum := Sum + New_Value;
+	   end loop;
+	end Consumer;
 
+begin 
+   null;
 end rts_lab1_3;
