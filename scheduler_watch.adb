a93e60a3cfb5617b2bdd7eb03a42f908a3d5d49d
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-13 11:41:53 +0200

    Watchdog working, changes time for F1 when F3 exceeds its time limit.

--- scheduler_watch.adb
+++ scheduler_watch.adb
@@ -1,18 +1,13 @@
 with Ada.Text_IO, Calendar;
 use Ada.Text_IO, Calendar;
 
-procedure Scheduler is
+procedure Scheduler_Watch is
    Short_Period : Duration := 0.5;
    Long_Period : Duration := 1.0;
    -- Start_Time : Time := Clock;
    -- Next_Time : Time := Start_Time + Short_Period;   
    Start_Time : Time;
    Next_Time : Time;
-   
-   task Main is
-      entry Exceeded;   
-      entry Not_Exceeded;
-   end Main;
    
    procedure F1 is
    begin
@@ -27,52 +22,52 @@ procedure Scheduler is
    end F2;
    
    procedure F3 is
-      task Watchdog;
+      task Main is
+	 entry Exceeded;
+	 entry Not_Exceeded;
+      end Main;
       
-      task body Watchdog is
+      task body Main is
       begin
-	 Put_Line("Watchdog running");
-	 delay Short_Period;
-	 Put_Line("Deadline exceeded!");
-	 Main.Exceeded;
-      end Watchdog;
+	 Put("F3 executing, time is now:");
+	 Put_Line(Duration'Image(Clock - Start_Time));
+	 delay Long_Period;
+	 select
+      	    accept Exceeded do
+	       Put_Line("Exceeded, in Main");
+	       Next_Time := Next_Time + Long_Period;
+      	    end Exceeded;
+      	 or
+      	    accept Not_Exceeded do
+	       Put_Line("Not exceeded, in Main");
+	    end Not_Exceeded;
+      	 end select;
+      end Main;
       
    begin
-      Put("F3 executing, time is now:");
-      Put_Line(Duration'Image(Clock - Start_Time));
-      delay Long_Period;
+      Put_Line("Watchdog running");
+      delay Short_Period;
+      Put_Line("Deadline exceeded!");
+      Main.Exceeded;
    end F3;
    
-   task body Main is
-   begin
-      Start_Time := Clock;
-      Next_Time := Start_Time + Short_Period;
-      loop
-	 F1;
-	 F2;
-	 
-	 delay until Next_Time;
-	 Next_Time := Next_Time + Short_Period;
-	 F3;
-	 delay until Next_Time;
-	 select
-	    accept Exceeded do
-	       Next_Time := Next_Time + Long_Period;
-	       delay until Next_Time;
-	    end Exceeded;
-	 or
-	    accept Not_Exceeded;
-	 end select;
-	 
-	 Next_Time := Next_Time + Long_Period;
-	 F1;
-	 F2;
-	 delay until Next_Time;
-	 Next_Time := Next_Time + Short_Period;
-      end loop;
-   end Main;
-   
-   
 begin
-   Put_Line("Running");
-end Scheduler;
+   Start_Time := Clock;
+   Next_Time := Start_Time + Short_Period;
+   loop
+      F1;
+      F2;
+      delay until Next_Time;
+      
+      Next_Time := Next_Time + Short_Period;
+      F3;
+      delay until Next_Time;
+      
+      Next_Time := Next_Time + Long_Period;
+      F1;
+      F2;
+      delay until Next_Time;
+      
+      Next_Time := Next_Time + Short_Period;
+   end loop;
+end Scheduler_Watch;
