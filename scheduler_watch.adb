50167ff280e4635b21748e3cd7f78cf941b2ad9d
Jessica <jessica121353@gmail.com>
2015-09-13 23:27:48 +0200

    I think is the correct answer!!!!!!

--- scheduler_watch.adb
+++ scheduler_watch.adb
@@ -4,7 +4,8 @@ use Ada.Text_IO, Calendar, Ada.Numerics.Float_Random;
 
 procedure Scheduler_Watch is
    Short_Period : Duration := 0.5;
-   Long_Period : Duration := 1.5;
+   Long_Period : Duration := 1.0;
+   little_time : Duration := 0.1;
    -- Start_Time : Time := Clock;
    -- Next_Time : Time := Start_Time + Short_Period;   
    Start_Time : Time;
@@ -35,21 +36,16 @@ procedure Scheduler_Watch is
       begin
 	 while Running = True loop
 	    select
-	       
 	       accept Stop do
 		  Running := False;
 	       end Stop;
-	       
 	    else
-	       
 	       if Clock > Alarm_Time then
 		  Put_Line("Deadline exceeded!");
 		  Next_Time := Next_Time + Long_Period;
 		  -- presupposes that F3 always takes less than 1 second
-		  
 		  Running := False;
-	       end if;
-	       
+	       end if;  
 	    end select;
 	 end loop;
       end Watchdog;
@@ -57,9 +53,9 @@ procedure Scheduler_Watch is
    begin
       Put("F3 executing, time is now:");
       Put_Line(Duration'Image(Clock - Start_Time));
-      if(F3exeutedtime > 0.5) then
-         delay 0.5;
-      else delay 0.1;
+      if(F3exeutedtime < 0.5) then
+         delay little_time;
+      else delay Short_Period;
       end if;
       --delay Long_Period;
       Watchdog.Stop;
@@ -75,17 +71,26 @@ begin
       F1;
       F2;
       delay until Next_Time;
+      
       reset (G);
       F3exeutedtime := Random(G);
       if (F3exeutedtime>0.5) then
             Put_Line("F3 executed more than 0.5.");
-            Next_Time := Next_Time + long_period;
+            -- Next_Time := Next_Time + Long_Period;
       else
             Put_Line("F3 executed less than 0.5.");
-            Next_Time := Next_Time + short_period;
-        end if;
+            -- Next_Time := Next_Time + Short_Period;
+      end if;
+
+      Next_Time := Next_Time + Short_Period;
       F3;
-        delay until Next_Time;
-        Next_Time := Next_Time + short_period;
+      delay until Next_Time;
+      
+      Next_Time := Next_Time + Long_Period;
+      F1;
+      F2;
+      delay until Next_Time;
+      
+      Next_Time := Next_Time + Short_Period;
    end loop;
 end Scheduler_Watch;
