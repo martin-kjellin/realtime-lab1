b57f00f5b14331f33d93b2941f07b58bb7b2d0ef
blue945u <blue945u@gmail.com>
2015-09-13 14:33:16 +0200

    Buffer compeleted

--- rts_lab1_3.adb
+++ rts_lab1_3.adb
@@ -9,31 +9,32 @@ use Ada.Numerics.Float_Random;
 
 procedure rts_lab1_3 is
 	task buffer is
-		entry put(X: in integer)
+		entry put(x: in integer)
 		entry get(x: out integer)
 	end;
 	task body buffer is
-		b: array (0 .. 15);
+		size: constant integer :=15;
+		b: array (1..size);
 		front: integer := 0;
-		number: integer := 0;
-		size: integer :=15;
+		number: integer range 0..size := 0;
 	begin
 		loop 
 		accept put(x: in integer)
 			do
 				if(number < size)
-					b(pointer):= x;
-					pointer = (pointer + 1) mod size;
+					b((front + number)mod size + 1):= x;
+					number = number + 1;
 				else
-					Put_Line("Buffer Full");
+					Put_Line("Buffer is Full");
 			end put;
 		accept get(x: out integer) 
 			do 
-				if(pointer < size)
-					b(pointer):= x;
-					pointer = (pointer + 1) mod size;
+				if(number > 0)
+					b(front mod size):= x;
+					number = number - 1;
+					front = front + 1;
 				else
-					Put_Line("Buffer Full");
+					Put_Line("Buffer is empty");
 			end get;
 		end loop;
 	end buffer;
