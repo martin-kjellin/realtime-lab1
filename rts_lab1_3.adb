59f4ab5944e2d8b58658df5006cc25703c8f1d05
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-15 23:06:48 +0200

    Cleaned up part 3, included random delays.

--- rts_lab1_3.adb
+++ rts_lab1_3.adb
@@ -1,45 +1,51 @@
+-- Lab 1, part 3
 with Ada.Text_IO;
 use Ada.Text_IO;
 with Ada.Numerics.Discrete_Random;
+with Ada.Numerics.Float_Random;
 
-procedure rts_lab1_3 is
+-- Inserts integers into a FIFO buffer and gets them from the buffer.
+procedure RTS_Lab1_3 is
    package Integer_Random is new Ada.Numerics.Discrete_Random(Integer);
-   use Integer_Random;
    
-   task Buffer is -- Declarations of the Buffer
+   task Buffer is
       entry Put(X : in integer);
       entry Get(X : out integer);
       entry Stop;
    end;
    
-   task Producer is -- Declarations of the Producer
+   task Producer is
       entry Stop;
    end;
    
-   task Consumer; -- Declarations of the Consumer
+   task Consumer;
    
-   task body Buffer is -- Definition of Buffer task
-      Size : constant Integer := 15; -- Size of the Buffer
-      B : array (1..Size) of Integer; -- Container of the Buffer
-      Front : Integer := 0; -- Point to the top position of the Buffer
-      Number : Integer range 0..Size := 0; -- The number of integers in the Buffer
-      Running : Boolean := True; -- keep running Buffer(True) or not(False)
+   -- The buffer.
+   task body Buffer is
+      Size : constant Integer := 15; -- Size of the buffer.
+      B : array (1..Size) of Integer; -- The actual buffer.
+      Front : Integer := 0; -- Decides the next position to use in the buffer.
+      Number : Integer range 0..Size := 0; -- The number of integers
+                                           -- in the buffer.
+      Running : Boolean := True; -- The buffer should be stopped when the
+                                 -- consumer finishes executing.
    begin
       while Running = True loop
 	 select
-	    when Number < Size => -- if the Buffer is not full
+	    when Number < Size => -- If the buffer is not full.
+	       -- Insert an integer.
 	       accept Put(X : in Integer) do
-	       B((Front + Number) mod Size + 1) := X; -- put x on the bottom 
-	       Number := Number + 1; -- The number of integers in the Buffer plus one
+	       B((Front + Number) mod Size + 1) := X;
+	       Number := Number + 1;
 	       end Put;
 	       
 	 or
 
-	    when Number > 0 => -- if the Buffer is not empty
+	    when Number > 0 => -- If the buffer is not empty.
 	       accept Get(X : out Integer) do 
-	       X := B(Front mod Size + 1); -- get x from the top of the buffer
-	       Number := Number - 1; -- The number of integers in the Buffer minus one 
-	       Front := Front + 1; -- Point to the next top position of the Buffer
+	       X := B(Front mod Size + 1);
+	       Number := Number - 1;
+	       Front := Front + 1;
 	       end Get;
 	       
 	 or
@@ -50,41 +56,46 @@ procedure rts_lab1_3 is
 	    
 	 end select;
       end loop;
-   end buffer;
+   end Buffer;
    
-   task body Producer is -- Definition of Producer task
-      G : Generator;
-      Produce_Range : constant Integer := 26; -- maximun of the random number
-      New_Value : Integer; -- New random number produced
-      Running : Boolean := True; -- keep running Producer(True) or not(False)
+   -- Inserts values in the range 0..25 into the buffer.
+   task body Producer is
+      F : Ada.Numerics.Float_Random.Generator;
+      G : Integer_Random.Generator;
+      New_Value : Integer;
+      Running : Boolean := True; -- The producer should be stopped when the
+                                 -- consumer finishes executing.
    begin
-      Reset(G);
+      Ada.Numerics.Float_Random.Reset(F);
+      Integer_Random.Reset(G);
       while Running = True loop
 	 select
 	    accept Stop do
 	       Running := False;
 	    end Stop;
-	    
 	 else
-	    
-	    New_Value := Random(G) mod Produce_Range; -- Produce random number from 0 to 25
-	    Put("Producer Put to buffer:");
+	    New_Value := Integer_Random.Random(G) mod 26;
+	    Put("Producer inserts:");
 	    Put_Line(Integer'Image(New_Value));
-	    Buffer.Put(New_Value); -- Put number into buffer
-	    
+	    Buffer.Put(New_Value);
+	    delay Duration(Ada.Numerics.Float_Random.Random(F));
 	 end select;
       end loop;
    end Producer;
    
-   task body Consumer is -- Definition of Producer task
+   -- Gets values from the buffer and summarizes them until the sum is over 100.
+   task body Consumer is
+      E : Ada.Numerics.Float_Random.Generator;
       Sum : Integer := 0;
       New_Value : Integer;
    begin
-      while Sum <= 100 loop -- loop while summerizes in consumer no more than 100
-	 Buffer.Get(New_Value); -- get number from buffer
-	 Put("Consumer get from buffer:");
+      Ada.Numerics.Float_Random.Reset(E);
+      while Sum <= 100 loop
+	 Buffer.Get(New_Value);
+	 Put("Consumer gets:");
 	 Put_Line(Integer'Image(New_Value));
-	 Sum := Sum + New_Value; -- summerized
+	 Sum := Sum + New_Value;
+	 delay Duration(Ada.Numerics.Float_Random.Random(E));
       end loop;
       Producer.Stop;
       Buffer.Stop;
@@ -92,4 +103,4 @@ procedure rts_lab1_3 is
 
 begin 
    null;
-end rts_lab1_3;
+end RTS_Lab1_3;
